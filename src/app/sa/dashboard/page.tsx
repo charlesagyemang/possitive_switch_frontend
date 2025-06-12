5babc92ae90d282447b62fa5303a6b8d81e9178b
frimpongopoku <pongofrimi@gmail.com>
2025-06-12 23:14:21 +0000

    feat: update SadminDashboard title and enhance PageTitle component with custom icon support

--- src/app/sa/dashboard/page.tsx
+++ src/app/sa/dashboard/page.tsx
@@ -2,13 +2,15 @@
 import SuperAdminRoot from "@/app/shared/wrappers/sadmin-root";
 import SadminSpace from "@/app/shared/wrappers/sadmin-space";
 import PageTitle from "@/components/built/text/page-title";
+import { Smile } from "lucide-react";
 
 export default function SadminDashboard() {
   return (
     <SuperAdminRoot>
       <SadminSpace>
         <PageTitle
-          title="Welcome, Mr Frimpong"
+          // Icon={Smile}
+          title="Welcome, Mr Admin"
           description="You are a super admin, manage all the companies and candidates from here!"
         />
       </SadminSpace>
--- src/components/built/text/page-title.tsx
+++ src/components/built/text/page-title.tsx
@@ -1,18 +1,26 @@
 import { IconNode, type LucideIcon } from "lucide-react";
-import React from "react";
+import React, { ReactNode } from "react";
 
 function PageTitle({
   title,
   description,
   Icon,
+  customIcon,
 }: {
   title?: string;
   description?: string;
   Icon?: LucideIcon;
+  customIcon?: () => any;
 }) {
+  const renderIcon = () => {
+    if (customIcon) return customIcon();
+
+    return Icon && <Icon className="mr-4 h-14 w-14 text-gray-300" />;
+  };
   return (
     <div className="flex items-center">
-      {Icon && <Icon className="mr-4 h-14 w-14 text-gray-300" />}
+      {renderIcon()}
+      {/* {Icon && <Icon className="mr-4 h-14 w-14 text-gray-300" />} */}
       <div>
         <h2 className="text-3xl font-bold text-primary">{title}</h2>
         <p className="mt-1 text-sm text-gray-500">{description}</p>
